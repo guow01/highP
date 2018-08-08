d7519983eaf2cd9d2e9ba570189dc1e020f4a1af
guow <449311536@qq.com>
2018-08-08 23:42:04 +0800

    我的项目

--- src/main/java/com/highP/highP/Application.java
+++ src/main/java/com/highP/highP/Application.java
@@ -1,7 +1,12 @@
 package com.highP.highP;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        // spring应用启动
+        SpringApplication.run(Application.class,args);
     }
 }
